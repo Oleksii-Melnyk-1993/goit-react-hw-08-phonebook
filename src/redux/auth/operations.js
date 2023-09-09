8f334ed479adb6cfd4b139e9fd71f2fe976f0308
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-09 22:31:38 +0300

    finish auth thunk

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -17,6 +17,50 @@ export const register = createAsyncThunk(
     try {
       const values = await axios.post('/user/signup', references);
       setAuthenticationHeader(values.data.token);
-    } catch (error) {}
+      return values.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (references, thunkApi) => {
+    try {
+      const values = await axios.post('/user/login', references);
+      setAuthenticationHeader(values.data.token);
+      return values.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+  try {
+    const values = await axios.post('/user/logout');
+    clearAuthenticationHeader();
+    return values.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.message);
+  }
+});
+
+export const refreshPage = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkApi) => {
+    const state = thunkApi.getState();
+    const persistToken = state.auth.token;
+    if (persistToken === null) {
+      return thunkApi.rejectWithValue('Please, login!');
+    }
+    try {
+      setAuthenticationHeader(persistToken);
+      const values = await axios.get('/users/current');
+      return values.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
   }
 );
