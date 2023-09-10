a1d60b12697baec3503c2a8b35804b04aff1b63f
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-10 22:40:34 +0300

    add signupform

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,1 +1,39 @@
-export const RegisterForm = () => {};
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSumbit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        passwrd: form.elements.passwrd.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={handleSumbit} autoComplete="off">
+      <label>
+        {' '}
+        Login
+        <input type="text" name="name" />
+      </label>{' '}
+      Email
+      <label>
+        <input type="email" name="email" />
+      </label>
+      <label>
+        {' '}
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
