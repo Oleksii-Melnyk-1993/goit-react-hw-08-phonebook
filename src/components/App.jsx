ea4aa529634ad19e58ac06bf3cb7e4db0d893add
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-11 23:00:16 +0300

    add Contact page and change App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
-import css from './App.module.css';
-
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import {
-  selectContactsCount,
-  selectError,
-  selectIsLoading,
-} from 'redux/contacts/selectors';
-import { Toaster } from 'react-hot-toast';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/operations';
-import { Audio } from 'react-loader-spinner';
 import { useAuthentication } from 'hooks';
 import { refreshPage } from 'redux/auth/operations';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import Home from 'pages/Home/Home';
+import { RestrictedRoute } from './RestrictedRoute';
+import Login from 'pages/Login/Login';
+import { PrivateRoute } from './PrivateRoute';
+import Contacts from 'pages/Contacts/Contacts';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -24,40 +18,25 @@ export const App = () => {
     dispatch(refreshPage());
   }, [dispatch]);
 
-  return (
-    <div
-      style={{
-        paddingTop: '60px',
-        paddingBottom: '60px',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        color: '#010101',
-      }}
-    >
-      <h1 className={css.title}>Phonebook</h1>
-      <ContactForm />
-      <h2 className={css.subtitle}>Contacts</h2>
-      <p className={css.total}>
-        Total contacts: <span className={css.total_count}>{totalCount}</span>
-      </p>
-      <Filter />
-      {isLoading && !error && (
-        <Audio
-          height="80"
-          width="80"
-          radius="9"
-          color="green"
-          ariaLabel="loading"
-          wrapperStyle
-          wrapperClass
+  return isRefresh ? (
+    <b>Login user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+          }
         />
-      )}
-      {error && <p> An {error} has occured😥, try again later </p>}
-      <ContactList />
-      <Toaster position="top-right" />
-    </div>
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,62 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import css from './Contacts.module.css';
+import { Filter } from 'components/Filter/Filter';
+import { Audio } from 'react-loader-spinner';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Toaster } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectContactsCount,
+  selectError,
+  selectIsLoading,
+} from 'redux/contacts/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+
+export default function Contacts() {
+  const dispatch = useDispatch();
+  const totalCount = useSelector(selectContactsCount);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts(), [dispatch]);
+  });
+
+  return (
+    <div
+      style={{
+        paddingTop: '60px',
+        paddingBottom: '60px',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#010101',
+      }}
+    >
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <h2 className={css.subtitle}>Contacts</h2>
+      <p className={css.total}>
+        Total contacts: <span className={css.total_count}>{totalCount}</span>
+      </p>
+      <Filter />
+      {isLoading && !error && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      )}
+      {error && <p> An {error} has occured😥, try again later </p>}
+      <ContactList />
+      <Toaster position="top-right" />
+    </div>
+  );
+}
