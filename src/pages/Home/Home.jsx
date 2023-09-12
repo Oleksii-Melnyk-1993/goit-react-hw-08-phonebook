f8c2cd5faac5534f85c474f173eb654e5159f5bf
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-12 22:54:49 +0300

    making css for pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Phonebook</h1>
+    <div
+      style={{
+        minHeight: 'calc(100vh - 100px)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 25,
+      }}
+    >
+      <h1 style={{ fontSize: 48, textAlign: 'center' }}>Phonebook</h1>
     </div>
   );
 }
