05f776eb26c41664ebc85bc3bb62344212d3185f
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-14 20:50:58 +0300

    add replace

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -3,5 +3,5 @@ import { Navigate } from 'react-router-dom';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLogIn = useAuthentication();
-  return isLogIn ? <Navigate to={redirectTo} /> : Component;
+  return isLogIn ? <Navigate to={redirectTo} replace /> : Component;
 };
